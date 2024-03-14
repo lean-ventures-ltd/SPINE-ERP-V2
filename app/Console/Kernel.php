633a40307e276d4f43b6aba53eb57cebe6cb1295
Dohd <alloicebrian@gmail.com>
2024-03-14 15:34:01 +0300

    Update schedule to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('software-proforma:generate')->everyMinute();
+        $schedule->command('software-proforma:generate')->everyDay();
     }
 
     /**
