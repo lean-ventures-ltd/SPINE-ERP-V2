f71abac2d602087b3277bc0663c59077fe59f49c
Dohd <alloicebrian@gmail.com>
2024-05-02 08:32:39 +0300

    Update inventory product import logic

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -4,15 +4,13 @@ namespace App\Imports;
 
 use App\Models\product\Product;
 use App\Models\product\ProductVariation;
+use App\Models\productvariable\Productvariable;
 use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
-use Illuminate\Validation\Rule;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
-use Maatwebsite\Excel\Row;
+use Illuminate\Validation\ValidationException;
 
 class ProductsImport implements ToCollection, WithBatchInserts, WithValidation, WithStartRow
 {
@@ -22,7 +20,6 @@ class ProductsImport implements ToCollection, WithBatchInserts, WithValidation, 
      * @return \Illuminate\Database\Eloquent\Model|null
      */
     private $rows = 0;
-    private $records;
 
     private $data;
 
@@ -31,54 +28,60 @@ class ProductsImport implements ToCollection, WithBatchInserts, WithValidation, 
         $this->data = $data;
     }
 
-
     public function collection(Collection $rows)
     {
-          if (isset($this->data['category'])) $category = $this->data['category']; else return false;
-           if (isset($this->data['warehouse'])) $warehouse = $this->data['warehouse']; else return false;
-        ++$this->rows;
-        foreach ($rows as $row) {
-            if (count($row) == 13) {
-                $product = new  Product([
+        // dd($rows);
+        if (empty($this->data['category_id']) || empty($this->data['warehouse_id']))
+            throw ValidationException::withMessages(['Category or Warehouse is required!']);
+            
+        $category_id = $this->data['category_id'];
+        $warehouse_id = $this->data['warehouse_id'];
+
+        foreach ($rows as $key => $row) {
+            $row_num = $key+1;
+            if ($row_num == 1 && $row->count() < 13) {
+                throw ValidationException::withMessages(['Missing columns! Use latest CSV file template.']);
+            } elseif ($row_num > 1) {
+                if (empty($row[0])) throw ValidationException::withMessages(['Product Name is required on row no. $row_num']);
+                if (empty($row[3])) throw ValidationException::withMessages(['Unit is required on row no. $row_num']);
+                
+                $unit = Productvariable::where(['code' => $row[3], 'unit_type' => 'base'])->first();
+                $product = Product::create([
+                    'productcategory_id' => $category_id,
                     'name' => $row[0],
-                    'productcategory_id' => $category,
-                    'taxrate' => $row[1],
-                    'product_des' => $row[2],
-                    'unit' => $row[3],
+                    'taxrate' => numberClean($row[1]),
+                    'product_des' => empty($row[2])? $row[0] : $row[2],
+                    'unit_id' => $unit? $unit->id : null,
                     'code_type' => $row[10],
-                    'ins' => auth()->user()->ins
-
+                    'ins' => $this->data['ins'],
                 ]);
-                $product->save();
-
-                $relate = $product->standard()->create([
-                    'warehouse_id' => $warehouse,
+                $productvar = ProductVariation::create([
+                    'parent_id' => $product->id,
+                    'name' => $product->name,
+                    'warehouse_id' => $warehouse_id,
                     'code' => $row[4],
-                    'price' => $row[5],
-                    'purchase_price' => $row[6],
-                    'disrate' => $row[7],
-                    'qty' => $row[8],
-                    'alert' => $row[9],
+                    'price' => numberClean($row[5]),
+                    'purchase_price' => numberClean($row[6]),
+                    'disrate' => numberClean($row[7]),
+                    'qty' => numberClean($row[8]),
+                    'alert' => numberClean($row[9]),
                     'barcode' => $row[11],
-                    'expiry' => $row[12],
-                    'ins' => auth()->user()->ins
+                    'expiry' => date_for_database($row[12]),
+                    'ins' => $product->ins,
                 ]);
-            }
-            else {
-               return false;
-           }
+                if (in_array('productcategory_id', \Schema::getColumnListing('product_variations'))) {
+                    $productvar->update(['productcategory_id' => $category_id]);
+                }
+                ++$this->rows;
+            }            
         }
-
-
     }
-
-
 
     public function rules(): array
     {
         return [
             '0' => 'required|string',
-            '1' => 'required',
+            '3' => 'required|string',
         ];
     }
 
@@ -94,6 +97,6 @@ class ProductsImport implements ToCollection, WithBatchInserts, WithValidation, 
 
     public function startRow(): int
     {
-        return 2;
+        return 1;
     }
 }
--- resources/views/focus/import/partials/products.blade.php
+++ resources/views/focus/import/partials/products.blade.php
@@ -1,35 +1,31 @@
-{{ Form::open(['route' => ['biller.import.general','products'], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post','files' => true, 'id' => 'import-data']) }}
-<input type="hidden" name="update" value="1">
-{!! Form::file('import_file', array('class'=>'form-control input col-md-6 mb-1' )) !!}
-<div class="row">
-    <div class="col-md-4">
-        <div class="form-group">
-            {{ Form::label( 'productcategory_id', trans('products.productcategory_id'),['class' => 'col control-label']) }}
-            <div class='col'>
-                <select class="form-control" name="productcategory" id="product_cat">
-                    @foreach($data['product_category'] as $item)
-                        @if(!$item->c_type)
-                            <option value="{{$item->id}}" {{ $item->id === @$products->productcategory_id ? " selected" : "" }}>{{$item->title}}</option>
-                        @endif
-                    @endforeach
-
-                </select>
+{{ Form::open(['route' => ['biller.import.general', 'products'], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'files' => true, 'id' => 'import-data']) }}
+    <input type="hidden" name="update" value="1">
+    {!! Form::file('import_file', array('class'=>'form-control input col-md-6 mb-1' )) !!}
+    <div class="row">
+        <div class="col-md-4">
+            <div class="form-group">
+                {{ Form::label( 'productcategory_id', trans('products.productcategory_id'),['class' => 'col control-label']) }}
+                <div class='col'>
+                    <select class="form-control" name="category_id" id="product_cat" required>
+                        @foreach($data['product_categories'] as $item)
+                            <option value="{{$item->id}}">{{$item->title}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="form-group">
+                {{ Form::label('productcategory_id', 'Product Location',['class' => 'col control-label']) }}
+                <div class='col'>
+                    <select class="form-control" name="warehouse_id" id="warehouse" required>
+                        @foreach($data['warehouses'] as $item)
+                            <option value="{{$item->id}}">{{$item->title}}</option>
+                        @endforeach
+                    </select>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="form-group">
-            {{ Form::label( 'productcategory_id', trans('products.warehouse_id'),['class' => 'col control-label']) }}
-            <div class='col'>
-                <select class="form-control" name="warehouse">
-
-                    @foreach($data['warehouses'] as $item)
-                        <option value="{{$item->id}}" {{ $item->id === @$products->warehouse_id ? " selected" : "" }}>{{$item->title}}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-    </div>
-</div>
-{{ Form::submit(trans('import.upload_import'), ['class' => 'btn btn-primary btn-md']) }}
+    {{ Form::submit(trans('import.upload_import'), ['class' => 'btn btn-primary btn-md']) }}
 {{ Form::close() }}
