5be5712ed5cb028941500e321572dc6f8f6594db
robertmwenja4 <robertmwenja4@gmail.com>
2024-06-05 13:01:18 +0300

    Project Notes recreated

--- resources/views/focus/projects/modal/note_new.blade.php
+++ resources/views/focus/projects/modal/note_new.blade.php
@@ -2,7 +2,40 @@
      aria-hidden="true">
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content ">
-            <section class="todo-form">
+            {{-- <section class="todo-form"> --}}
+                @if (isset($note))
+                    <form id="data_form_note" class="todo-input">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="AddNoteLabel">Edit Note</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <fieldset class="form-group col-12">
+                                    <input type="text" class="new-todo-item form-control required" placeholder="{{trans('general.title')}}" value="{{$note->title}}" name="title">
+                                </fieldset>
+                            </div>
+                            <fieldset class="form-group">
+                                <textarea class="new-todo-item form-control summernote required" placeholder="{{trans('tasks.description')}}" rows="6"  name="content">{{strip_tags($note->content)}}</textarea>
+                            </fieldset>
+                        </div>
+                        <input type="hidden" value="{{ route('biller.projects.update_meta') }}" id="action-url_6">
+                        <input type="hidden" value="{{ $project->id }}" name="project_id">
+                        <input type="hidden" value="{{ $note->id }}" name="object_id">
+                        <input type="hidden" value="6" name="obj_type">
+                        <div class="modal-footer">
+                            <fieldset class="form-group position-relative has-icon-left mb-0">
+                                <button type="button" id="submit-data_note" class="btn btn-info add-todo-item" data-dismiss="modal">
+                                    <i class="fa fa-paper-plane-o d-block d-lg-none"></i>
+                                    <span class="d-none d-lg-block">Update</span>
+                                </button>
+                            </fieldset>
+                        </div>
+                        
+                    </form>
+                @else
                 <form id="data_form_note" class="todo-input">
                     <div class="modal-header">
                         <h5 class="modal-title" id="AddNoteLabel">{{trans('general.note')}}</h5>
@@ -32,7 +65,8 @@
                     <input type="hidden" value="{{ $project->id }}" name="project_id">
                     <input type="hidden" value="6" name="obj_type">
                 </form>
-            </section>
+                @endif
+            {{-- </section> --}}
         </div>
     </div>
 </div>
--- resources/views/focus/projects/tabs/note.blade.php
+++ resources/views/focus/projects/tabs/note.blade.php
@@ -1,20 +1,60 @@
 <div class="tab-pane" id="tab_data6" aria-labelledby="tab6" role="tabpanel">
-    <button type="button" class="btn btn-info float-right mr-2" data-toggle="modal" data-target="#AddNoteModal">
+    <button type="button" class="btn btn-info mr-2" id="addNote" data-toggle="modal" data-target="#AddNoteModal">
         <i class="fa fa-plus-circle"></i> Note
     </button>
-    <div class="card-body">
-        <table id="notes-table" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>{{ trans('general.title') }}</th>
-                    <th>Content</th>
-                    <th>Created By</th>
-                    <th>Created At</th>
-                    <th>{{ trans('general.action') }}</th>
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
-    </div>
+    <ul class="timeline">
+        @php
+            $flag = true;
+            $total = count($project->project_notes);
+        @endphp
+        @foreach ($project->project_notes as $row)
+            <li class="{!! (!$flag)? 'timeline-inverted' : '' !!} {{ $row->user_type == 'customer' ? 'timeline-item-right' : 'timeline-item-left' }}" id="m_{{$row['id']}}">
+                <div class="timeline-badge" style="background-color:@if ($row->user_type == 'customer') #000   @else #0b97f4  @endif;">
+                    {{$row->user_type}}
+                </div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">{{$row['title']}}</h4>
+                    </div>
+                        <div class="timeline-body mb-1">
+                            <p>{{strip_tags($row['content'])}}</p>
+                            
+                        </div>
+                    <small class="text-muted"><i class="fa fa-user"></i>
+                        <strong>{{ @$row->creator->fullname }}</strong>
+                        <i class="fa fa-clock-o"></i> {{trans('general.created')}} {{dateTimeFormat($row['created_at'])}}
+                    </small>
+                    <div>
+                        <div class="btn-group">
+                            <button class="btn btn-link note-edit" obj-type="6" data-id="{{$row['id']}}" data-url="{{ route('biller.projects.edit_meta') }}">
+                                <i class="ft ft-edit" style="font-size: 1.2em"></i>
+                            </button>
+                            <button class="btn btn-link note-del" obj-type="6" data-id="{{$row['id']}}" data-url="{{ route('biller.projects.delete_meta') }}">
+                                <i class="fa fa-trash fa-lg danger"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            @php
+                $flag = !$flag;
+                $total--;
+            @endphp
+        @endforeach
+        
+    </ul>
+    
+    {{-- <ul class="timeline">
+        @foreach($project->project_notes as $message)
+          
+            <li class="timeline-item {{ $message->user_type == 'customer' ? 'timeline-item-right' : 'timeline-item-left' }}">
+                    <div class="timeline-badge" style="background-color:@if ($message->user_type == 'customer') #000   @else #0b97f4  @endif;">{{ $message->user_type }}</div>
+                    <div class="timeline-panel">
+                        <h5 class="timeline-title">{{ $message->title }}</h5>
+                        <p>{{ $message->content }}</p>
+                    </div>
+                </li>
+        @endforeach
+    </ul> --}}
+          
 </div>
