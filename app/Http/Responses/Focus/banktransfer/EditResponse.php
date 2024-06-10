a454fd1572bae46eb524518cbd3e0042a3bec5b6
Dohd <alloicebrian@gmail.com>
2024-06-10 09:04:50 +0300

    Remove banktransfer_rel and pmt_mode variables

--- app/Http/Responses/Focus/banktransfer/EditResponse.php
+++ app/Http/Responses/Focus/banktransfer/EditResponse.php
@@ -3,7 +3,6 @@
 namespace App\Http\Responses\Focus\banktransfer;
 
 use App\Models\account\Account;
-use App\Models\banktransfer\Banktransfer;
 use Illuminate\Contracts\Support\Responsable;
 
 class EditResponse implements Responsable
@@ -31,10 +30,8 @@ class EditResponse implements Responsable
     public function toResponse($request)
     {
         $accounts = Account::whereHas('accountType', fn($q) => $q->where('system', 'bank'))->get(['id', 'holder']);
-        $banktransfer_rel = Banktransfer::where('tid', $this->banktransfer->tid)->where('credit', '>', 0)->first();
-        $pmt_mode = current(explode(' - ', $this->banktransfer->note));
         
-        return view('focus.banktransfers.edit', compact('banktransfer_rel', 'accounts', 'pmt_mode'))
+        return view('focus.banktransfers.edit', compact('accounts'))
             ->with([ 'banktransfer' => $this->banktransfer]);
     }
 }
